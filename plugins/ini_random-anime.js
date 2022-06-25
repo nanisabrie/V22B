6086e8a4ee8c73f4a50469a30ae323f3b0a4dfd2
AyGemuy <91945119+AyGemuy@users.noreply.github.com>
2022-06-25 18:39:46 +0800

    New
    New

--- plugins/ini_random-anime.js
+++ plugins/ini_random-anime.js
@@ -1,178 +1,178 @@
 import axios from "axios"
 let handler = async (m, {command, conn}) => {
 if (command == 'cosplay') {
-let haha = await conn.getFile(`https://api-reysekha.herokuapp.com/api/random/cosplay?apikey=apirey`)
+let haha = await conn.getFile(`https://yourclown.eu.org/api/random/cosplay?apikey=apirey`)
 conn.sendButton(m.chat, `Anime *${command}*`.trim(), author, haha.data, [['🔄 Next 🔄', `/${command}`]], m)}
 if (command == 'akira') {
-let haha = await conn.getFile(`https://api-reysekha.herokuapp.com/api/wallpaper/akira?apikey=apirey`)
+let haha = await conn.getFile(`https://yourclown.eu.org/api/wallpaper/akira?apikey=apirey`)
 conn.sendButton(m.chat, `Anime *${command}*`.trim(), author, haha.data, [['🔄 Next 🔄', `/${command}`]], m)}
 if (command == 'akiyama') {
-let haha = await conn.getFile(`https://api-reysekha.herokuapp.com/api/wallpaper/akiyama?apikey=apirey`)
+let haha = await conn.getFile(`https://yourclown.eu.org/api/wallpaper/akiyama?apikey=apirey`)
 conn.sendButton(m.chat, `Anime *${command}*`.trim(), author, haha.data, [['🔄 Next 🔄', `/${command}`]], m)}
 if (command == 'ana') {
-let haha = await conn.getFile(`https://api-reysekha.herokuapp.com/api/wallpaper/ana?apikey=apirey`)
+let haha = await conn.getFile(`https://yourclown.eu.org/api/wallpaper/ana?apikey=apirey`)
 conn.sendButton(m.chat, `Anime *${command}*`.trim(), author, haha.data, [['🔄 Next 🔄', `/${command}`]], m)}
 if (command == 'asuna') {
-let haha = await conn.getFile(`https://api-reysekha.herokuapp.com/api/wallpaper/asuna?apikey=apirey`)
+let haha = await conn.getFile(`https://yourclown.eu.org/api/wallpaper/asuna?apikey=apirey`)
 conn.sendButton(m.chat, `Anime *${command}*`.trim(), author, haha.data, [['🔄 Next 🔄', `/${command}`]], m)}
 if (command == 'ayuzawa') {
-let haha = await conn.getFile(`https://api-reysekha.herokuapp.com/api/wallpaper/ayuzawa?apikey=apirey`)
+let haha = await conn.getFile(`https://yourclown.eu.org/api/wallpaper/ayuzawa?apikey=apirey`)
 conn.sendButton(m.chat, `Anime *${command}*`.trim(), author, haha.data, [['🔄 Next 🔄', `/${command}`]], m)}
 if (command == 'boneka-chucky') {
-let haha = await conn.getFile(`https://api-reysekha.herokuapp.com/api/wallpaper/boneka-chucky?apikey=apirey`)
+let haha = await conn.getFile(`https://yourclown.eu.org/api/wallpaper/boneka-chucky?apikey=apirey`)
 conn.sendButton(m.chat, `Anime *${command}*`.trim(), author, haha.data, [['🔄 Next 🔄', `/${command}`]], m)}
 if (command == 'boruto') {
-let haha = await conn.getFile(`https://api-reysekha.herokuapp.com/api/wallpaper/boruto?apikey=apirey`)
+let haha = await conn.getFile(`https://yourclown.eu.org/api/wallpaper/boruto?apikey=apirey`)
 conn.sendButton(m.chat, `Anime *${command}*`.trim(), author, haha.data, [['🔄 Next 🔄', `/${command}`]], m)}
 if (command == 'chiho') {
-let haha = await conn.getFile(`https://api-reysekha.herokuapp.com/api/wallpaper/chiho?apikey=apirey`)
+let haha = await conn.getFile(`https://yourclown.eu.org/api/wallpaper/chiho?apikey=apirey`)
 conn.sendButton(m.chat, `Anime *${command}*`.trim(), author, haha.data, [['🔄 Next 🔄', `/${command}`]], m)}
 if (command == 'chitoge') {
-let haha = await conn.getFile(`https://api-reysekha.herokuapp.com/api/wallpaper/chitoge?apikey=apirey`)
+let haha = await conn.getFile(`https://yourclown.eu.org/api/wallpaper/chitoge?apikey=apirey`)
 conn.sendButton(m.chat, `Anime *${command}*`.trim(), author, haha.data, [['🔄 Next 🔄', `/${command}`]], m)}
 if (command == 'deidara') {
-let haha = await conn.getFile(`https://api-reysekha.herokuapp.com/api/wallpaper/deidara?apikey=apirey`)
+let haha = await conn.getFile(`https://yourclown.eu.org/api/wallpaper/deidara?apikey=apirey`)
 conn.sendButton(m.chat, `Anime *${command}*`.trim(), author, haha.data, [['🔄 Next 🔄', `/${command}`]], m)}
 if (command == 'eba') {
-let haha = await conn.getFile(`https://api-reysekha.herokuapp.com/api/wallpaper/eba?apikey=apirey`)
+let haha = await conn.getFile(`https://yourclown.eu.org/api/wallpaper/eba?apikey=apirey`)
 conn.sendButton(m.chat, `Anime *${command}*`.trim(), author, haha.data, [['🔄 Next 🔄', `/${command}`]], m)}
 if (command == 'elaina') {
-let haha = await conn.getFile(`https://api-reysekha.herokuapp.com/api/wallpaper/elaina?apikey=apirey`)
+let haha = await conn.getFile(`https://yourclown.eu.org/api/wallpaper/elaina?apikey=apirey`)
 conn.sendButton(m.chat, `Anime *${command}*`.trim(), author, haha.data, [['🔄 Next 🔄', `/${command}`]], m)}
 if (command == 'emilia') {
-let haha = await conn.getFile(`https://api-reysekha.herokuapp.com/api/wallpaper/emilia?apikey=apirey`)
+let haha = await conn.getFile(`https://yourclown.eu.org/api/wallpaper/emilia?apikey=apirey`)
 conn.sendButton(m.chat, `Anime *${command}*`.trim(), author, haha.data, [['🔄 Next 🔄', `/${command}`]], m)}
 if (command == 'erza') {
-let haha = await conn.getFile(`https://api-reysekha.herokuapp.com/api/wallpaper/erza?apikey=apirey`)
+let haha = await conn.getFile(`https://yourclown.eu.org/api/wallpaper/erza?apikey=apirey`)
 conn.sendButton(m.chat, `Anime *${command}*`.trim(), author, haha.data, [['🔄 Next 🔄', `/${command}`]], m)}
 if (command == 'gremory') {
-let haha = await conn.getFile(`https://api-reysekha.herokuapp.com/api/wallpaper/gremory?apikey=apirey`)
+let haha = await conn.getFile(`https://yourclown.eu.org/api/wallpaper/gremory?apikey=apirey`)
 conn.sendButton(m.chat, `Anime *${command}*`.trim(), author, haha.data, [['🔄 Next 🔄', `/${command}`]], m)}
 if (command == 'hestia') {
-let haha = await conn.getFile(`https://api-reysekha.herokuapp.com/api/wallpaper/hestia?apikey=apirey`)
+let haha = await conn.getFile(`https://yourclown.eu.org/api/wallpaper/hestia?apikey=apirey`)
 conn.sendButton(m.chat, `Anime *${command}*`.trim(), author, haha.data, [['🔄 Next 🔄', `/${command}`]], m)}
 if (command == 'hinata') {
-let haha = await conn.getFile(`https://api-reysekha.herokuapp.com/api/wallpaper/hinata?apikey=apirey`)
+let haha = await conn.getFile(`https://yourclown.eu.org/api/wallpaper/hinata?apikey=apirey`)
 conn.sendButton(m.chat, `Anime *${command}*`.trim(), author, haha.data, [['🔄 Next 🔄', `/${command}`]], m)}
 if (command == 'inori') {
-let haha = await conn.getFile(`https://api-reysekha.herokuapp.com/api/wallpaper/inori?apikey=apirey`)
+let haha = await conn.getFile(`https://yourclown.eu.org/api/wallpaper/inori?apikey=apirey`)
 conn.sendButton(m.chat, `Anime *${command}*`.trim(), author, haha.data, [['🔄 Next 🔄', `/${command}`]], m)}
 if (command == 'isuzu') {
-let haha = await conn.getFile(`https://api-reysekha.herokuapp.com/api/wallpaper/isuzu?apikey=apirey`)
+let haha = await conn.getFile(`https://yourclown.eu.org/api/wallpaper/isuzu?apikey=apirey`)
 conn.sendButton(m.chat, `Anime *${command}*`.trim(), author, haha.data, [['🔄 Next 🔄', `/${command}`]], m)}
 if (command == 'itachi') {
-let haha = await conn.getFile(`https://api-reysekha.herokuapp.com/api/wallpaper/itachi?apikey=apirey`)
+let haha = await conn.getFile(`https://yourclown.eu.org/api/wallpaper/itachi?apikey=apirey`)
 conn.sendButton(m.chat, `Anime *${command}*`.trim(), author, haha.data, [['🔄 Next 🔄', `/${command}`]], m)}
 if (command == 'itori') {
-let haha = await conn.getFile(`https://api-reysekha.herokuapp.com/api/wallpaper/itori?apikey=apirey`)
+let haha = await conn.getFile(`https://yourclown.eu.org/api/wallpaper/itori?apikey=apirey`)
 conn.sendButton(m.chat, `Anime *${command}*`.trim(), author, haha.data, [['🔄 Next 🔄', `/${command}`]], m)}
 if (command == 'kaga') {
-let haha = await conn.getFile(`https://api-reysekha.herokuapp.com/api/wallpaper/kaga?apikey=apirey`)
+let haha = await conn.getFile(`https://yourclown.eu.org/api/wallpaper/kaga?apikey=apirey`)
 conn.sendButton(m.chat, `Anime *${command}*`.trim(), author, haha.data, [['🔄 Next 🔄', `/${command}`]], m)}
 if (command == 'kagura') {
-let haha = await conn.getFile(`https://api-reysekha.herokuapp.com/api/wallpaper/kagura?apikey=apirey`)
+let haha = await conn.getFile(`https://yourclown.eu.org/api/wallpaper/kagura?apikey=apirey`)
 conn.sendButton(m.chat, `Anime *${command}*`.trim(), author, haha.data, [['🔄 Next 🔄', `/${command}`]], m)}
 if (command == 'kaori') {
-let haha = await conn.getFile(`https://api-reysekha.herokuapp.com/api/wallpaper/kaori?apikey=apirey`)
+let haha = await conn.getFile(`https://yourclown.eu.org/api/wallpaper/kaori?apikey=apirey`)
 conn.sendButton(m.chat, `Anime *${command}*`.trim(), author, haha.data, [['🔄 Next 🔄', `/${command}`]], m)}
 if (command == 'keneki') {
-let haha = await conn.getFile(`https://api-reysekha.herokuapp.com/api/wallpaper/keneki?apikey=apirey`)
+let haha = await conn.getFile(`https://yourclown.eu.org/api/wallpaper/keneki?apikey=apirey`)
 conn.sendButton(m.chat, `Anime *${command}*`.trim(), author, haha.data, [['🔄 Next 🔄', `/${command}`]], m)}
 if (command == 'kotori') {
-let haha = await conn.getFile(`https://api-reysekha.herokuapp.com/api/wallpaper/kotori?apikey=apirey`)
+let haha = await conn.getFile(`https://yourclown.eu.org/api/wallpaper/kotori?apikey=apirey`)
 conn.sendButton(m.chat, `Anime *${command}*`.trim(), author, haha.data, [['🔄 Next 🔄', `/${command}`]], m)}
 if (command == 'kurumi') {
-let haha = await conn.getFile(`https://api-reysekha.herokuapp.com/api/wallpaper/kurumi?apikey=apirey`)
+let haha = await conn.getFile(`https://yourclown.eu.org/api/wallpaper/kurumi?apikey=apirey`)
 conn.sendButton(m.chat, `Anime *${command}*`.trim(), author, haha.data, [['🔄 Next 🔄', `/${command}`]], m)}
 if (command == 'loli') {
-let haha = await conn.getFile(`https://api-reysekha.herokuapp.com/api/wallpaper/loli?apikey=apirey`)
+let haha = await conn.getFile(`https://yourclown.eu.org/api/wallpaper/loli?apikey=apirey`)
 conn.sendButton(m.chat, `Anime *${command}*`.trim(), author, haha.data, [['🔄 Next 🔄', `/${command}`]], m)}
 if (command == 'madara') {
-let haha = await conn.getFile(`https://api-reysekha.herokuapp.com/api/wallpaper/madara?apikey=apirey`)
+let haha = await conn.getFile(`https://yourclown.eu.org/api/wallpaper/madara?apikey=apirey`)
 conn.sendButton(m.chat, `Anime *${command}*`.trim(), author, haha.data, [['🔄 Next 🔄', `/${command}`]], m)}
 if (command == 'megumin') {
-let haha = await conn.getFile(`https://api-reysekha.herokuapp.com/api/wallpaper/megumin?apikey=apirey`)
+let haha = await conn.getFile(`https://yourclown.eu.org/api/wallpaper/megumin?apikey=apirey`)
 conn.sendButton(m.chat, `Anime *${command}*`.trim(), author, haha.data, [['🔄 Next 🔄', `/${command}`]], m)}
 if (command == 'mikasa') {
-let haha = await conn.getFile(`https://api-reysekha.herokuapp.com/api/wallpaper/mikasa?apikey=apirey`)
+let haha = await conn.getFile(`https://yourclown.eu.org/api/wallpaper/mikasa?apikey=apirey`)
 conn.sendButton(m.chat, `Anime *${command}*`.trim(), author, haha.data, [['🔄 Next 🔄', `/${command}`]], m)}
 if (command == 'miku') {
-let haha = await conn.getFile(`https://api-reysekha.herokuapp.com/api/wallpaper/miku?apikey=apirey`)
+let haha = await conn.getFile(`https://yourclown.eu.org/api/wallpaper/miku?apikey=apirey`)
 conn.sendButton(m.chat, `Anime *${command}*`.trim(), author, haha.data, [['🔄 Next 🔄', `/${command}`]], m)}
 if (command == 'minato') {
-let haha = await conn.getFile(`https://api-reysekha.herokuapp.com/api/wallpaper/minato?apikey=apirey`)
+let haha = await conn.getFile(`https://yourclown.eu.org/api/wallpaper/minato?apikey=apirey`)
 conn.sendButton(m.chat, `Anime *${command}*`.trim(), author, haha.data, [['🔄 Next 🔄', `/${command}`]], m)}
 if (command == 'mobil') {
-let haha = await conn.getFile(`https://api-reysekha.herokuapp.com/api/wallpaper/mobil?apikey=apirey`)
+let haha = await conn.getFile(`https://yourclown.eu.org/api/wallpaper/mobil?apikey=apirey`)
 conn.sendButton(m.chat, `Anime *${command}*`.trim(), author, haha.data, [['🔄 Next 🔄', `/${command}`]], m)}
 if (command == 'montor') {
-let haha = await conn.getFile(`https://api-reysekha.herokuapp.com/api/wallpaper/montor?apikey=apirey`)
+let haha = await conn.getFile(`https://yourclown.eu.org/api/wallpaper/montor?apikey=apirey`)
 conn.sendButton(m.chat, `Anime *${command}*`.trim(), author, haha.data, [['🔄 Next 🔄', `/${command}`]], m)}
 if (command == 'naruto') {
-let haha = await conn.getFile(`https://api-reysekha.herokuapp.com/api/wallpaper/naruto?apikey=apirey`)
+let haha = await conn.getFile(`https://yourclown.eu.org/api/wallpaper/naruto?apikey=apirey`)
 conn.sendButton(m.chat, `Anime *${command}*`.trim(), author, haha.data, [['🔄 Next 🔄', `/${command}`]], m)}
 if (command == 'nezuko') {
-let haha = await conn.getFile(`https://api-reysekha.herokuapp.com/api/wallpaper/nezuko?apikey=apirey`)
+let haha = await conn.getFile(`https://yourclown.eu.org/api/wallpaper/nezuko?apikey=apirey`)
 conn.sendButton(m.chat, `Anime *${command}*`.trim(), author, haha.data, [['🔄 Next 🔄', `/${command}`]], m)}
 if (command == 'onepiece') {
-let haha = await conn.getFile(`https://api-reysekha.herokuapp.com/api/wallpaper/onepiece?apikey=apirey`)
+let haha = await conn.getFile(`https://yourclown.eu.org/api/wallpaper/onepiece?apikey=apirey`)
 conn.sendButton(m.chat, `Anime *${command}*`.trim(), author, haha.data, [['🔄 Next 🔄', `/${command}`]], m)}
 if (command == 'pokemon') {
-let haha = await conn.getFile(`https://api-reysekha.herokuapp.com/api/wallpaper/pokemon?apikey=apirey`)
+let haha = await conn.getFile(`https://yourclown.eu.org/api/wallpaper/pokemon?apikey=apirey`)
 conn.sendButton(m.chat, `Anime *${command}*`.trim(), author, haha.data, [['🔄 Next 🔄', `/${command}`]], m)}
 if (command == 'rize') {
-let haha = await conn.getFile(`https://api-reysekha.herokuapp.com/api/wallpaper/rize?apikey=apirey`)
+let haha = await conn.getFile(`https://yourclown.eu.org/api/wallpaper/rize?apikey=apirey`)
 conn.sendButton(m.chat, `Anime *${command}*`.trim(), author, haha.data, [['🔄 Next 🔄', `/${command}`]], m)}
 if (command == 'sagiri') {
-let haha = await conn.getFile(`https://api-reysekha.herokuapp.com/api/wallpaper/sagiri?apikey=apirey`)
+let haha = await conn.getFile(`https://yourclown.eu.org/api/wallpaper/sagiri?apikey=apirey`)
 conn.sendButton(m.chat, `Anime *${command}*`.trim(), author, haha.data, [['🔄 Next 🔄', `/${command}`]], m)}
 if (command == 'sakura') {
-let haha = await conn.getFile(`https://api-reysekha.herokuapp.com/api/wallpaper/sakura?apikey=apirey`)
+let haha = await conn.getFile(`https://yourclown.eu.org/api/wallpaper/sakura?apikey=apirey`)
 conn.sendButton(m.chat, `Anime *${command}*`.trim(), author, haha.data, [['🔄 Next 🔄', `/${command}`]], m)}
 if (command == 'sasuke') {
-let haha = await conn.getFile(`https://api-reysekha.herokuapp.com/api/wallpaper/sasuke?apikey=apirey`)
+let haha = await conn.getFile(`https://yourclown.eu.org/api/wallpaper/sasuke?apikey=apirey`)
 conn.sendButton(m.chat, `Anime *${command}*`.trim(), author, haha.data, [['🔄 Next 🔄', `/${command}`]], m)}
 if (command == 'shina') {
-let haha = await conn.getFile(`https://api-reysekha.herokuapp.com/api/wallpaper/shina?apikey=apirey`)
+let haha = await conn.getFile(`https://yourclown.eu.org/api/wallpaper/shina?apikey=apirey`)
 conn.sendButton(m.chat, `Anime *${command}*`.trim(), author, haha.data, [['🔄 Next 🔄', `/${command}`]], m)}
 if (command == 'shinka') {
-let haha = await conn.getFile(`https://api-reysekha.herokuapp.com/api/wallpaper/shinka?apikey=apirey`)
+let haha = await conn.getFile(`https://yourclown.eu.org/api/wallpaper/shinka?apikey=apirey`)
 conn.sendButton(m.chat, `Anime *${command}*`.trim(), author, haha.data, [['🔄 Next 🔄', `/${command}`]], m)}
 if (command == 'shinomiya') {
-let haha = await conn.getFile(`https://api-reysekha.herokuapp.com/api/wallpaper/shinomiya?apikey=apirey`)
+let haha = await conn.getFile(`https://yourclown.eu.org/api/wallpaper/shinomiya?apikey=apirey`)
 conn.sendButton(m.chat, `Anime *${command}*`.trim(), author, haha.data, [['🔄 Next 🔄', `/${command}`]], m)}
 if (command == 'shizuka') {
-let haha = await conn.getFile(`https://api-reysekha.herokuapp.com/api/wallpaper/shizuka?apikey=apirey`)
+let haha = await conn.getFile(`https://yourclown.eu.org/api/wallpaper/shizuka?apikey=apirey`)
 conn.sendButton(m.chat, `Anime *${command}*`.trim(), author, haha.data, [['🔄 Next 🔄', `/${command}`]], m)}
 if (command == 'shota') {
-let haha = await conn.getFile(`https://api-reysekha.herokuapp.com/api/wallpaper/shota?apikey=apirey`)
+let haha = await conn.getFile(`https://yourclown.eu.org/api/wallpaper/shota?apikey=apirey`)
 conn.sendButton(m.chat, `Anime *${command}*`.trim(), author, haha.data, [['🔄 Next 🔄', `/${command}`]], m)}
 if (command == 'tejina') {
-let haha = await conn.getFile(`https://api-reysekha.herokuapp.com/api/wallpaper/tejina?apikey=apirey`)
+let haha = await conn.getFile(`https://yourclown.eu.org/api/wallpaper/tejina?apikey=apirey`)
 conn.sendButton(m.chat, `Anime *${command}*`.trim(), author, haha.data, [['🔄 Next 🔄', `/${command}`]], m)}
 if (command == 'toukachan') {
-let haha = await conn.getFile(`https://api-reysekha.herokuapp.com/api/wallpaper/toukachan?apikey=apirey`)
+let haha = await conn.getFile(`https://yourclown.eu.org/api/wallpaper/toukachan?apikey=apirey`)
 conn.sendButton(m.chat, `Anime *${command}*`.trim(), author, haha.data, [['🔄 Next 🔄', `/${command}`]], m)}
 if (command == 'tsunade') {
-let haha = await conn.getFile(`https://api-reysekha.herokuapp.com/api/wallpaper/tsunade?apikey=apirey`)
+let haha = await conn.getFile(`https://yourclown.eu.org/api/wallpaper/tsunade?apikey=apirey`)
 conn.sendButton(m.chat, `Anime *${command}*`.trim(), author, haha.data, [['🔄 Next 🔄', `/${command}`]], m)}
 if (command == 'waifu2') {
-let haha = await conn.getFile(`https://api-reysekha.herokuapp.com/api/wallpaper/waifu2?apikey=apirey`)
+let haha = await conn.getFile(`https://yourclown.eu.org/api/wallpaper/waifu2?apikey=apirey`)
 conn.sendButton(m.chat, `Anime *${command}*`.trim(), author, haha.data, [['🔄 Next 🔄', `/${command}`]], m)}
 if (command == 'waifu') {
-let haha = await conn.getFile(`https://api-reysekha.herokuapp.com/api/wallpaper/waifu?apikey=apirey`)
+let haha = await conn.getFile(`https://yourclown.eu.org/api/wallpaper/waifu?apikey=apirey`)
 conn.sendButton(m.chat, `Anime *${command}*`.trim(), author, haha.data, [['🔄 Next 🔄', `/${command}`]], m)}
 if (command == 'yotsuba') {
-let haha = await conn.getFile(`https://api-reysekha.herokuapp.com/api/wallpaper/yotsuba?apikey=apirey`)
+let haha = await conn.getFile(`https://yourclown.eu.org/api/wallpaper/yotsuba?apikey=apirey`)
 conn.sendButton(m.chat, `Anime *${command}*`.trim(), author, haha.data, [['🔄 Next 🔄', `/${command}`]], m)}
 if (command == 'yuki') {
-let haha = await conn.getFile(`https://api-reysekha.herokuapp.com/api/wallpaper/yuki?apikey=apirey`)
+let haha = await conn.getFile(`https://yourclown.eu.org/api/wallpaper/yuki?apikey=apirey`)
 conn.sendButton(m.chat, `Anime *${command}*`.trim(), author, haha.data, [['🔄 Next 🔄', `/${command}`]], m)}
 if (command == 'yumeko') {
-let haha = await conn.getFile(`https://api-reysekha.herokuapp.com/api/wallpaper/yumeko?apikey=apirey`)
+let haha = await conn.getFile(`https://yourclown.eu.org/api/wallpaper/yumeko?apikey=apirey`)
 conn.sendButton(m.chat, `Anime *${command}*`.trim(), author, haha.data, [['🔄 Next 🔄', `/${command}`]], m)}
 if (command == 'yuri') {
-let haha = await conn.getFile(`https://api-reysekha.herokuapp.com/api/wallpaper/yuri?apikey=apirey`)
+let haha = await conn.getFile(`https://yourclown.eu.org/api/wallpaper/yuri?apikey=apirey`)
 conn.sendButton(m.chat, `Anime *${command}*`.trim(), author, haha.data, [['🔄 Next 🔄', `/${command}`]], m)}
 }
 
