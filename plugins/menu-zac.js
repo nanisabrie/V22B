6242f4067e0e31104e4d08edbc1a0df8ab463b61
AyGemuy <91945119+AyGemuy@users.noreply.github.com>
2022-06-26 20:27:28 +0800

    New
    New

---
+++ plugins/menu-zac.js
@@ -0,0 +1,201 @@
+import axios from 'axios'
+import FormData from 'form-data'
+import fetch from 'node-fetch'
+
+let handler = async(m, { conn, groupMetadata, usedPrefix, text, args, isPrems, isOwner, command }) => {
+let frep = { contextInfo: { externalAdReply: {title: global.wm, body: global.author, sourceUrl: snh, thumbnail: fs.readFileSync('./thumbnail.jpg')}}}
+let fdoc = {quoted:{key : {participant : '0@s.whatsapp.net'},message: {documentMessage: {title: `${command}`}}}}
+let imgr = flaaa.getRandom()
+
+let urut = text.split`|`
+  let one = urut[1]
+  let two = urut[2]
+  let three = urut[3]
+  
+let template = (args[0] || '').toLowerCase()
+if (!args[0]) {
+let caption = `*Contoh Penggunaan*
+${usedPrefix + command} cecan
+`
+conn.sendButton(m.chat, caption, wm, null, [
+                ['Menu', `${usedPrefix}menu`]
+            ], m, fdoc)
+            }
+            
+if (command) {
+switch (template) {
+
+        case 'cecan':
+case 'china':
+case 'hijaber':
+case 'indonesia':
+case 'japan':
+case 'korea':
+case 'loli':
+case 'malaysia':
+case 'random':
+case 'santuy':
+case 'thailand':
+case 'ukhty':
+case 'vietnam':
+            if (!one) throw `Contoh penggunaan ${usedPrefix}${command} cecan
+*List Command*
+• cecan
+• china
+• hijaber
+• indonesia
+• japan
+• korea
+• loli
+• malaysia
+• random
+• santuy
+• thailand
+• ukhty
+• vietnam
+`
+        let as= `https://api.zacros.my.id/asupan/${one}`
+        let ass = `*Result:* ${one}`
+        await conn.sendButton(m.chat, ass, wm, as, [
+                ['Next', `${usedPrefix + command} ${one}`]
+            ], m, fdoc)
+            break
+            case 'alphacoders':
+            case 'wallpapercave':
+            case 'konachan':
+            if (!two) throw `Contoh penggunaan ${usedPrefix}${command} alphacoders|teks
+*List Command*
+• alphacoders
+• wallpapercave
+• konachan
+`
+        let wl = `https://api.zacros.my.id/search/${one}?query=${two}`
+        let wll = `*Result:* ${one}`
+        await conn.sendButton(m.chat, wll, wm, wl, [
+                ['Next', `${usedPrefix + command} ${one}`]
+            ], m, fdoc)
+            break
+            case 'pinterest':
+            if (!one) throw `Contoh penggunaan ${usedPrefix}${command} query`
+        let pn = await fetch(`https://api.zacros.my.id/search/pinterest?query=${one}`)
+        let pnn = await pn.json()
+        let pnnn = pnn.result
+        let pnnnn = `*Result:* ${one}`
+        await conn.sendButton(m.chat, pnnnn, wm, pnnn.getRandom(), [
+                ['Next', `${usedPrefix + command} ${one}`]
+            ], m, fdoc)
+            break
+            case 'sstick':
+            if (!one) throw `Contoh penggunaan ${usedPrefix}${command} query`
+        let sp = await fetch(`https://api.zacros.my.id/search/sticker?query=${one}`)
+        let spp = await sp.json()
+        let sppp = `*Result:* ${spp.result[0].title}
+        ${spp.result[0].url}
+`
+        await conn.sendButton(m.chat, sppp, wm, null, [
+                ['Next', `${usedPrefix + command} ${one}`]
+            ], m, fdoc)
+            break
+            case 'google':
+            if (!one) throw `Contoh penggunaan ${usedPrefix}${command} query`
+        let go = await fetch(`https://api.zacros.my.id/search/google?query=${one}`)
+        let goo = await go.json()
+        let gooo = `*Result:* ${goo.result[0].title}
+        ${goo.result[0].link}
+`
+        await conn.sendButton(m.chat, gooo, wm, null, [
+                ['Next', `${usedPrefix + command} ${one}`]
+            ], m, fdoc)
+            break
+            case 'hmod':
+            if (!one) throw `Contoh penggunaan ${usedPrefix}${command} query`
+        let hm = await fetch(`https://api.zacros.my.id/search/google?query=${one}`)
+        let hmm = await hm.json()
+        let hmmm = `*Result:* ${hmm.result[0].title}
+        ${hmm.result[0].link}
+`
+        await conn.sendButton(m.chat, hmmm, wm, hmm.icon, [
+                ['Next', `${usedPrefix + command} ${one}`]
+            ], m, fdoc)
+            break
+            case 'wiki':
+            if (!one) throw `Contoh penggunaan ${usedPrefix}${command} query`
+        let wk = await fetch(`https://api.zacros.my.id/search/google?query=${one}`)
+        let wkk = await wk.json()
+        let wkkk = `*Result:* ${wkk.result[0].wiki}
+        ${wkk.result[0].judul}
+`
+        await conn.sendButton(m.chat, wkkk, wm, wkk.thumb, [
+                ['Next', `${usedPrefix + command} ${one}`]
+            ], m, fdoc)
+            break
+            case 'milf':
+            case 'waifu':
+            case 'husbu':
+            case 'loli':
+            case 'cosplay':
+            case 'darkjokes':
+            case 'meme':
+            if (!one) throw `Contoh penggunaan ${usedPrefix}${command} query`
+        let rimg = `https://api.zacros.my.id/randomimg/${one}`
+        let riimg = `*Result:* ${one}
+`
+        await conn.sendButton(m.chat, riimg, wm, rimg, [
+                ['Next', `${usedPrefix + command} ${one}`]
+            ], m, fdoc)
+            break
+            case 'ppcouple':
+            if (!one) throw `Contoh penggunaan ${usedPrefix}${command} query`
+        let pp = await fetch(`https://api.zacros.my.id/randomimg/${one}`)
+        let ppc = await pp.json()
+        let cwo = `*Cwok:* ${one}`
+        await conn.sendButton(m.chat, cwo, wm, ppc.male, [
+                ['Next', `${usedPrefix + command} ${one}`]
+            ], m, fdoc)
+            let cwe = `*Cwok:* ${one}`
+        await conn.sendButton(m.chat, cwe, wm, ppc.female, [
+                ['Next', `${usedPrefix + command} ${one}`]
+            ], m, fdoc)
+            break
+            case 'quotes':
+        let qtt = await qt.json()
+        let qttt = `*Result:* ${qtt.result.quotes}
+        ${qtt.result.author}
+`
+        await conn.sendButton(m.chat, qttt, wm, null, [
+                ['Next', `${usedPrefix + command}`]
+            ], m, fdoc)
+            break
+            case 'quotesanime':
+        let qm = await fetch(`https://api.zacros.my.id/randomtext/quotesanime`)
+        let qmm = await qm.json()
+        let qmmm = `*Result:* ${qmm.result.char_name}
+        ${qmm.result.anime}
+        ${qmm.result.quotes}
+        ${qmm.result.episode}
+`
+        await conn.sendButton(m.chat, qmmm, qmm.result.date, qmm.result.img, [
+                ['Next', `${usedPrefix + command}`]
+            ], m, fdoc)
+            break
+            case 'faktaunik':
+            case 'fml':
+            case 'cerpen':
+            case 'katagalau':
+            case 'truth':
+            case 'dare':
+            if (!one) throw `Contoh penggunaan ${usedPrefix}${command} query`
+        let fa = await fetch(`https://api.zacros.my.id/randomtext/${one}`)
+        let faa = await fa.json()
+        let faaa = `*Result:* ${faa.result}`
+        await conn.sendButton(m.chat, faaa, wm, null, [
+                ['Next', `${usedPrefix + command}`]
+            ], m, fdoc)
+            break
+}
+}
+}
+handler.help = ['zac <command> <teks>']
+handler.tags = ['tools'] 
+handler.command = /^zac$/i
+export default handler
--- plugins/random-command12.js
+++ plugins/random-command12.js
@@ -1,24 +1,78 @@
-import fetch from 'node-fetch'
+
+import fetch = from 'node-fetch'
+import { servers, ytv } = from '../lib/y2mate'
+import xfar = from 'xfarr-api'
+import hxz = from "hxz-api"
+import axios = from 'axios'
+let limit = 1024354
 import fs from 'fs'
 
-let handler = async(m, { conn, groupMetadata, usedPrefix, text, args, command }) => {
+let handler = async(m, { conn, groupMetadata, usedPrefix, text, args, isPrems, isOwner, command }) => {
 let frep = { contextInfo: { externalAdReply: {title: global.wm, body: global.author, sourceUrl: snh, thumbnail: fs.readFileSync('./thumbnail.jpg')}}}
 let fdoc = {quoted:{key : {participant : '0@s.whatsapp.net'},message: {documentMessage: {title: `${command}`}}}}
 let imgr = flaaa.getRandom()
 
-if (command == 'chara') {
-let f = await fetch(`https://betabotz-api.herokuapp.com/api/game/tebakchara?apikey=BetaBotz`)
-let res = await f.json()
-await conn.sendButton(m.chat, `*Result:*
-*Title:* ${res.result.name}
-*Name:* ${res.result.desc}
-*Caption:* ${res.result.url}
-`, wm, `${res.result.image}`, [
-                ['Next', `${usedPrefix + command}`]
-            ], m, fdoc)
+if (command == 'ttdownloader') {
+let p = await  hxz.ttdownloader(text)
+const { nowm, wm, audio } = p
+conn.sendFile(m.chat, nowm, null, 'Nih', m)
 }
+
+if (command == 'ytamp3') {
+if (!args || !args[0]) throw 'Uhm... urlnya mana?'
+  let chat = global.db.data.chats[m.chat]
+  let server = (args[1] || servers[0]).toLowerCase()
+  let { dl_link, thumb, title, filesize, filesizeF} = await yta(args[0], servers.includes(server) ? server : servers[0])
+  let isLimit = (isPrems || isOwner ? 99 : limit) * 1024 < filesize 
+  if (!isLimit) conn.sendFile(m.chat, dl_link, title + '.mp3', `
+┏┉━━━━━━━━━━━❏
+┆ *YOUTUBE MP3*
+├┈┈┈┈┈┈┈┈┈┈┈
+┆• *Judul:* ${title}
+│• *Type:* MP3
+┆• *📥 Ukuran File:* ${filesizeF}
+└❏
+`.trim(), m, null, {
+  asDocument: chat.useDocument
+})
 }
-handler.command = handler.help = ['chara']
-handler.tags = ['random']
+
+if (command == 'ytvmp4') {
+if (!args || !args[0]) throw 'Uhm... urlnya mana?'
+  let chat = global.db.data.chats[m.chat]
+  let server = (args[1] || servers[0]).toLowerCase()
+  let { dl_link, thumb, title, filesize, filesizeF} = await ytv(args[0], servers.includes(server) ? server : servers[0])
+  let isLimit = (isPrems || isOwner ? 99 : limit) * 1024 < filesize
+  let name = await conn.getName(m.sender)
+let wm = global.wm
+  conn.sendFile(m.chat, thumb, 'thumbnail.jpg', `
+*Title:* ${title}
+*Filesize:* ${filesizeF}
+*${isLimit ? 'Pakai ': ''}Link:* ${dl_link}
+`.trim(), m)
+  let _thumb = {}
+  try { _thumb = { thumbnail: await (await fetch(thumb)).buffer() } }
+  catch (e) { }
+  if (!isLimit) conn.sendFile(m.chat, dl_link, title + '.mp4', `
+*Title:* ${title}
+*Filesize:* ${filesizeF}
+`.trim(), m, false, {
+  ..._thumb,
+  asDocument: chat.useDocument
+})
+}
+
+if (command == 'ytv720') {
+if (!text) throw 'Masukkan Link\n\nContoh: .ytv720 https://youtube.com/xxxxxx'
+  let res = await xfar.Youtube(text)
+m.reply('*Tunggu Sebentar...*')
+conn.sendFile(m.chat,res.medias[2].url, '', `Youtube Downloader
+720p
+jika masih blur, berarti videonya emang ngeblur dari youtubenya
+Atau bisa jadi faktor *MUKA*`, m)
+}
+
+handler.command = handler.help = ['ttdownloader', 'ytamp3', 'ytvmp4', 'ytv720']
+handler.tags = ['downloader']
 
 export default handler
