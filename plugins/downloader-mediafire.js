da190b13ce3304fdb6a2e5a7aca78ca53a094f17
AyGemuy <91945119+AyGemuy@users.noreply.github.com>
2022-06-26 16:21:51 +0800

    New
    New

--- plugins/downloader-mediafire.js
+++ plugins/downloader-mediafire.js
@@ -1,5 +1,8 @@
+import { lookup } from 'mime-types'
 import { mediafiredl } from '@bochilteam/scraper'
+
 let handler = async (m, { conn, args, usedPrefix, command }) => {
+try {
     if (!args[0]) throw `Use example ${usedPrefix}${command} https://www.mediafire.com/file/941xczxhn27qbby/GBWA_V12.25FF-By.SamMods-.apk/file`
     let res = await mediafiredl(args[0])
     let { url, url2, filename, ext, aploud, filesize, filesizeH } = res
@@ -11,6 +14,15 @@ let handler = async (m, { conn, args, usedPrefix, command }) => {
 `.trim()
     m.reply(caption)
     await conn.sendFile(m.chat, url, filename, '', m, null, { mimetype: ext, asDocument: true })
+} catch {
+if (!args[0]) throw 'Input URL' 
+	if (!/https?:\/\/(www\.)?mediafire\.com/.test(args[0])) throw 'Invalid URL' 
+	let res = await mediafiredl(args[0])
+	let mimetype = await lookup(res.url)
+	delete res.url2
+	m.reply(Object.keys(res).map(v => `*• ${v.capitalize()}:* ${res[v]}`).join('\n') + '\n\n_Sending file..._')
+	conn.sendMessage(m.chat, { document: { url: res.url }, fileName: res.filename, mimetype }, { quoted: m })
+}
 }
 handler.help = ['mediafire'].map(v => v + ' <url>')
 handler.tags = ['downloader']
--- plugins/youtube-ytv.js
+++ plugins/youtube-ytv.js
@@ -3,6 +3,7 @@ import fetch from 'node-fetch'
 import { youtubedl, youtubedlv2, youtubedlv3 } from '@bochilteam/scraper'
 
 let handler = async (m, { conn, args, isPrems, isOwner }) => {
+try {
   if (args && /(?:https?:\/{2})?(?:w{3}|m|music)?\.?youtu(?:be)?\.(?:com|be)(?:watch\?v=|\/)([^\s&]+)/i.test(args[0])) {
     let opt = args[1] && args[1].isNumber() ? args[1].replace(/\D/g, '') : ''
     let res = await fetch(`https://yt-downloader.akkun3704.repl.co/yt?url=${args[0]}`)
@@ -10,7 +11,7 @@ let handler = async (m, { conn, args, isPrems, isOwner }) => {
     if (!res) res = ''
     let { description, ownerChannelName, viewCount, uploadDate, likes, dislikes } = res.result.videoDetails
     let { thumbnail, video: _video, title } = await youtubedlv2(args[0]).catch(async _ => await youtubedl(args[0])).catch(async _ => await youtubedlv3(args[0]))
-    await m.reply('Meluncur...')
+    //await m.reply('Meluncur...')
     let limitedSize = (isPrems || isOwner ? 99 : limit) * 1024
     let video, quality, link, lastError, isLimit //, source
     for (let i in _video) {
@@ -39,12 +40,51 @@ let handler = async (m, { conn, args, isPrems, isOwner }) => {
       await conn.sendMessage(m.chat, { image: { url: thumbnail }, caption }, { quoted: msg })
     })
   } else throw 'Invalid URL'
+  } catch {
+  if (!args || !args[0]) throw 'Uhm... urlnya mana?'
+  let chat = global.db.data.chats[m.chat]
+  const isY = /y(es)/gi.test(args[1])
+  const { thumbnail, video: _video, title } = await youtubedl(args[0]).catch(async _ => await youtubedlv2(args[0])).catch(async _ => await youtubedlv3(args[0]))
+  const limitedSize = ( limit) * 1024
+  let video, source, res, link, lastError, isLimit
+  for (let i in _video) {
+    try {
+      video = _video[i]
+      isLimit = limitedSize < video.fileSize
+       if (isLimit) continue
+      link = await video.download()
+      if (link) res = await fetch(link)
+      isLimit = res?.headers.get('content-length') && parseInt(res.headers.get('content-length')) < limitedSize
+      if (isLimit) continue
+      if (res) source = await res.arrayBuffer()
+      if (source instanceof ArrayBuffer) break
+    } catch (e) {
+      video = source = link = null
+      lastError = e
+    }
+  }
+  if ((!(source instanceof ArrayBuffer) || !link || !res.ok) && !isLimit) throw 'Error: ' + (lastError || 'Can\'t download video')
+  let _thumb = {}
+  try { _thumb = { thumbnail: await (await fetch(thumbnail)).buffer() } }
+  catch (e) { }
+  if (!isY && !isLimit) m.reply(isLimit ? `Size ${video.filesizeH}\nUkuran file diatas ${limit} MB, download sendiri: ${audio}` : wait)
+  if (!isLimit) await conn.sendFile(m.chat, link, title + '.mp4', `
+*📌Title:* ${title}
+*🗎 Filesize:* ${video.fileSizeH}
+
+Noh 
+`.trim(), m, false, {
+    ..._thumb,
+    asDocument: true
+  })
+  }
 }
-handler.help = ['ytmp4']
+handler.help = ['mp4', 'v', ''].map(v => 'yt' + v + ` <url> <without message>`)
 handler.tags = ['downloader']
-handler.alias = ['yt', 'ytv', 'ytmp4']
 handler.command = /^yt(v|mp4)?$/i
+
 handler.exp = 0
+
 
 export default handler
 
