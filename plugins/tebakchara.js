7f5186b69ad14b193e2c6dea868ffea842c558bf
AyGemuy <91945119+AyGemuy@users.noreply.github.com>
2022-06-26 08:02:30 +0800

    New
    New

---
+++ plugins/tebakchara.js
@@ -0,0 +1,42 @@
+import fetch from 'node-fetch'
+let timeout = 120000
+let poin = 4999
+let handler = async (m, { conn, usedPrefix }) => {
+    conn.tebakchara = conn.tebakchara ? conn.tebakchara : {}
+    let id = m.chat
+    if (id in conn.tebakchara) {
+        conn.sendButton(m.chat, 'Masih ada soal belum terjawab di chat ini', author, null, buttons, conn.tebakchara[id][0])
+        throw false
+    }
+    let res = await fetch('https://betabotz-api.herokuapp.com/api/game/tebakchara?apikey=BetaBotz')
+    let x = await res.json()
+    let json = x.result
+    let caption = `
+    Siapakah karakter ini
+    ${json.name}
+    
+Timeout *${(timeout / 1000).toFixed(2)} detik*
+Ketik ${usedPrefix}hcha untuk hint
+Bonus: ${poin} XP
+    `.trim()
+    conn.tebakchara[id] = [
+        await conn.sendButton(m.chat, caption, author, json.image, buttons, m),
+        json, poin,
+        setTimeout(() => {
+            if (conn.tebakchara[id]) conn.sendButton(m.chat, `Waktu habis!\nJawabannya adalah *${json.name}\nDesk : ${json.desc}*`, author, null, [
+                ['tebakchara', '/tebakchara']
+            ], conn.tebakchara[id][0])
+            delete conn.tebakchara[id]
+        }, timeout)
+    ]
+}
+handler.help = ['tebakchara']
+handler.tags = ['game']
+handler.command = /^tebakchara/i
+
+export default handler
+
+const buttons = [
+    ['Hint', '/hcha'],
+    ['Nyerah', 'menyerah']
+]
