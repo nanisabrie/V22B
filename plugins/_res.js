816dbf8c88d034be0c0a2e07927822a9ee787e78
AyGemuy <91945119+AyGemuy@users.noreply.github.com>
2022-06-27 19:43:52 +0800

    New
    New

--- plugins/_res.js
+++ plugins/_res.js
@@ -9,16 +9,13 @@ export async function all(m, { conn, text }) {
     // ketika ditag 
     try {
         if (m.mentionedJid.includes(this.user.jid) && m.isGroup) {
-        let user = global.db.data.users[m.sender]
-    const cooldown = 86400000
-    if (new Date - user.pc < cooldown) return // setiap 24 jam sekali
+    let user = global.db.data.users[m.sender]
     let pp = await conn.profilePictureUrl(who).catch(_ => './src/avatar_contact.png')
     let who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
     let name = await conn.getName(who)
     let caption = `👋 Hai *${name} @${who.split("@")[0]}*, ${user.banned ? 'kamu dibanned' : `Ada yang bisa *${this.user.name}* bantu?\nKetik *.menu* untuk melihat list fitur bot\nKetik *.join* untuk memasukkan bot ke grup kamu`}
-`.trim()
-    this.sendButton(m.chat, caption, wm, pp, [[user.banned ? 'Pemilik Bot' : 'Menu', user.banned ? '.owner' : '/menu']], m, { mentions: this.parseMention(caption) })
-    user.pc = new Date * 1
+    `.trim()
+    this.sendButton(m.chat, caption, global.wm, pp, [[user.banned ? 'Pemilik Bot' : 'Menu', user.banned ? '.owner' : '/menu']], m, { mentions: this.parseMention(caption) })
         }
     } catch (e) {
         return
