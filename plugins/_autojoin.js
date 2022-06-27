09901d48bc779507ea880bd2153df7cd2c8796c1
AyGemuy <91945119+AyGemuy@users.noreply.github.com>
2022-06-27 08:39:26 +0800

    New
    New

--- plugins/_autojoin.js
+++ plugins/_autojoin.js
@@ -1,5 +1,4 @@
 let isJoin = /chat.whatsapp.com\/([0-9A-Za-z]{20,24})( [0-9]{1,3})?/i
-import fs from 'fs'
 
 export async function before(m, { conn, args, usedPrefix, command, isAdmin, isBotAdmin }) {
     if (m.isBaileys && m.fromMe)
@@ -10,23 +9,15 @@ export async function before(m, { conn, args, usedPrefix, command, isAdmin, isBo
     const isAutoJoin = isJoin.exec(m.text)
 
     if (chat.autoJoin && isAutoJoin) {
-        await conn.sendButton(m.chat, `*Link Join Terdeteksi!*`, author, ['off autojoin', '/disable autojoin'], m)
-        let frep = { contextInfo: { externalAdReply: {title: global.wm, body: global.author, sourceUrl: snh, thumbnail: fs.readFileSync('./thumbnail.jpg')}}}
-        let imgr = flaaa.getRandom()
+        await conn.sendButton(m.chat, `*Link Join Terdeteksi!*`, wm, ['off autojoin', '/disable autojoin'], m)
     let [_, code, expired] = text.match(isJoin) || []
     if (!code) throw 'Link invalid'
     let res = await conn.groupAcceptInvite(code)
-    if (!res) throw res.toString()
     let name = await conn.getName(res).catch(_ => null)
-    expired = Math.floor(Math.min(5, Math.max(999, isOwner ? expired && expired.isNumber() ? parseInt(expired) : 0 : 3)))
-    let caption = `*Berhasil join grup* ${name || res} ${expired ? `selama *${expired}* hari` : ''}\n*Jangan lupa baca rules ngap!*`
-    await conn.sendButton(m.chat, caption, wm, imgr, [
+    let caption = `*Berhasil join grup* ${name || res} \nJangan lupa baca rules ngap!*`
+    await conn.sendButton(m.chat, caption, wm, null, [
                 ['Rules', `${usedPrefix}rules`]
-            ], m, frep)
-            
-    let chats = global.db.data.chats[res]
-    if (!chats) chats = global.db.data.chats[res] = {}
-    if (expired) chats.expired = +new Date() + expired * 1000 * 60 * 60 * 24
-    }
+            ], m)
+     }
     return !0
 }
