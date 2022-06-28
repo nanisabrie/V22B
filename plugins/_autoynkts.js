24eded01b2363b0a21d317443b9d79dd397f6066
AyGemuy <91945119+AyGemuy@users.noreply.github.com>
2022-06-28 11:50:49 +0800

    New
    New

--- plugins/_autoynkts.js
+++ plugins/_autoynkts.js
@@ -1,25 +1,38 @@
 import fetch from "node-fetch"
 import { readFileSync } from "fs"
 
-let handler = async (m, { conn, text }) => {
-let pp = await this.profilePictureUrl(m.sender, 'image').catch(_ => 'https://telegra.ph/file/24fa902ead26340f3df2c.png')
+
+
+export async function all(m) {
+
+  //Kalo mau menggokil pake ini
+  let pp = await this.profilePictureUrl(m.sender, 'image').catch(_ => 'https://telegra.ph/file/24fa902ead26340f3df2c.png')
+
   let stc = readFileSync('./ynkts.webp')
-  await this.sendMessage(m.chat, { sticker : stc, thumbnail: await( await fetch(pp)).buffer() , contextInfo:{  externalAdReply: { showAdAttribution: true,
+
+if (m.isBaileys) return
+    if (m.chat.endsWith('broadcast')) return
+
+    // ketika ditag 
+    try {
+        if (m.text.includes('?') && m.isGroup) {
+            await this.sendMessage(m.chat, { sticker : stc, thumbnail: await( await fetch(pp)).buffer() , contextInfo:{  externalAdReply: { showAdAttribution: true,
 mediaType:  1,
 mediaUrl: 'https://wa.me/6282195322106',
 title: '「 ❔ 」',
 body: wm,
 sourceUrl: 'http://github.com/AyGemuy', thumbnail: await( await fetch(pp)).buffer()
+ //Kalo mau ada pp dari sender/pengirimnya
+ //Kalo mau ringan pake global.thumb                                                                                                              
+                                                                                                               
+          //thumbnail: await( await fetch(pp)).buffer()
   }
  }}, { quoted: m })
-  }
-handler.customPrefix = /(?)/i
-handler.command = new RegExp
-  
-handler.mods = false
+          
+        }
+    } catch (e) {
+        return
+    }
 
-export default handler
-
-  function pickRandom(list) {
-     return list[Math.floor(Math.random() * list.length)]
-  }
+                                
+}
